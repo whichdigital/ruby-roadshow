487ffacf7583b5a3971b4d1d4c78f479679e9941
Chris Patuzzo <chris@patuzzo.co.uk>
2014-12-26 15:07:30 +0000

    Breadth-first traversal

--- solutions/03-christmas-tree/chris/lib/tree/base.rb
+++ solutions/03-christmas-tree/chris/lib/tree/base.rb
@@ -9,4 +9,16 @@ class Tree
   def initialize(root)
     @root = root
   end
+
+  def breadth_first
+    Enumerator.new do |y|
+      queue = [root]
+
+      until queue.empty?
+        node = queue.shift
+        y.yield(node)
+        queue += node.children
+      end
+    end
+  end
 end
--- solutions/03-christmas-tree/chris/spec/tree_spec.rb
+++ solutions/03-christmas-tree/chris/spec/tree_spec.rb
@@ -30,4 +30,28 @@ describe Tree do
     end
     expect(result).to eq(["an", "open"])
   end
+
+  it "works for extension a in the readme" do
+    tree = Tree.parse('
+
+                  chestnuts
+                  /     \
+              roasting   on
+              /    \       \
+            an     open     fire
+            / \       \        \
+          jack frost   nipping  on
+                                / \
+                              your nose
+
+    ')
+
+    expect(tree.breadth_first).to be_an(Enumerator)
+
+    result = tree.breadth_first.map do |node|
+      node.value
+    end
+
+    expect(result).to eq %w(chestnuts roasting on an open fire jack frost nipping on your nose)
+  end
 end
