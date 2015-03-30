391ce64d19c2edac6710654aac9a6f36c7b916b1
emre-basala <emrebasala@gmail.com>
2015-03-30 16:36:18 +0100

    added player class

---
+++ solutions/04-rock-paper-scissors/shared/lib/player.rb
@@ -0,0 +1,18 @@
+class Player
+  attr_reader :name, :colour
+
+  def initialize(name:, colour:)
+    @name = name
+    @colour = colour
+  end
+
+  def choice
+    choices[rand(0)]
+  end
+
+  private
+
+  def choices
+    %i(rock paper scissors)
+  end
+end
---
+++ solutions/04-rock-paper-scissors/shared/spec/player_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+require "./lib/player"
+
+RSpec.describe Player do
+  subject { Player.new(name: "Bob", colour: "#F00") }
+
+  describe "#choice" do
+    it "is either rock paper or scissors" do
+      expect([:rock, :paper, :scissors]).to include(subject.choice)
+    end
+  end
+end
